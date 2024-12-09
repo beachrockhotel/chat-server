bbf5aa98c89d83c2db0cb22243dfb62377b58240
beachrockhotel <d.komarov2019@gmail.com>
2024-12-09 21:59:15 +0300

    generation

--- cmd/grpc_client/main.go
+++ cmd/grpc_client/main.go
@@ -1,37 +1,11 @@
-package main
+package grpc_client
 
 import (
-	"context"
-	"log"
-	"time"
+	"fmt"
 
-	desc "github.com/beachrockhotel/chat-server/pkg/chat_v1"
 	"github.com/fatih/color"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-)
-
-const (
-	address = "localhost:50051"
-	authID  = 12
 )
 
 func main() {
-	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		log.Fatalf("failed to connect to server: %v", err)
-	}
-	defer conn.Close()
-
-	c := desc.NewChatV1Client(conn)
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-
-	r, err := c.Get(ctx, &desc.GetRequest{Id: authID})
-	if err != nil {
-		log.Fatalf("failed to get user by id: %d", chatID)
-	}
-
-	log.Printf(color.RedString("User info:\n") + color.GreenString("%+v", r.GetInfo()))
+	fmt.Println(color.GreenString("Hello, world!"))
 }
--- cmd/grpc_server/main.go
+++ cmd/grpc_server/main.go
@@ -1,74 +1,11 @@
-package main
+package grpc_client
 
 import (
-	"context"
 	"fmt"
-	"google.golang.org/protobuf/types/known/emptypb"
-	"log"
-	"net"
 
-	"github.com/brianvoe/gofakeit"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
-	"google.golang.org/protobuf/types/known/timestamppb"
-
-	desc "github.com/beachrockhotel/chat-server/pkg/chat_v1"
+	"github.com/fatih/color"
 )
 
-const grpcPort = 50051
-
-type server struct {
-	desc.UnimplementedChatV1Server
-}
-
-func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
-	log.Printf("Auth id: %d", req.GetId())
-
-	return &desc.GetResponse{
-		Id: req.GetId(),
-		Info: &desc.UserInfo{
-			Name:  gofakeit.Name(),
-			Email: gofakeit.Email(),
-			Role:  desc.Role_USER,
-		},
-		CreatedAt: timestamppb.New(gofakeit.Date()),
-		UpdatedAt: timestamppb.New(gofakeit.Date()),
-	}, nil
-}
-
-func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
-	log.Printf("Create user %v", req.GetInfo().GetName())
-
-	return &desc.CreateResponse{
-		Id: gofakeit.Int64(),
-	}, nil
-}
-
-func (s *server) Update(ctx context.Context, req *desc.UpdateRequest) (*emptypb.Empty, error) {
-	log.Printf("Updating user ID: %d, Name: %v, Email: %v", req.GetId(), req.GetName(), req.GetEmail())
-
-	return &emptypb.Empty{}, nil
-}
-
-func (s *server) Delete(ctx context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
-	log.Printf("Deleting user ID: %d", req.GetId())
-
-	return &emptypb.Empty{}, nil
-}
-
 func main() {
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-
-	s := grpc.NewServer()
-	reflection.Register(s)
-	desc.RegisterAuthV1Server(s, &server{})
-
-	log.Printf("server listening at %v", lis.Addr())
-
-	if err = s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
+	fmt.Println(color.GreenString("Hello, world!"))
 }
